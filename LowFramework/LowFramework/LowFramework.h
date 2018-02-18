81da4e21c665688c8e09d8ce43ea118d125e8aa8
Inficare Pvt. Ltd <inficare@Inficares-Mac-mini.local>
2018-02-18 15:14:59 +0545

    initial commit

---
+++ LowFramework/LowFramework/LowFramework.h
@@ -0,0 +1,19 @@
+//
+//  LowFramework.h
+//  LowFramework
+//
+//  Created by Inficare Pvt. Ltd. on 18/02/2018.
+//  Copyright © 2018 Inficare Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LowFramework.
+FOUNDATION_EXPORT double LowFrameworkVersionNumber;
+
+//! Project version string for LowFramework.
+FOUNDATION_EXPORT const unsigned char LowFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LowFramework/PublicHeader.h>
+
+
---
+++ LowFramework/LowFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  LowFramework
+//
+//  Created by Inficare Pvt. Ltd. on 18/02/2018.
+//  Copyright © 2018 Inficare Pvt. Ltd. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service{
+    private init(){}
+    
+    public static func getMessage() ->String{
+        return "It is a string"
+    }
+}
